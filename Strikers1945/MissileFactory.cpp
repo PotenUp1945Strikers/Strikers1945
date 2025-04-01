f3f775e8d4a62586c8944f6e81f7e3525961c3cc
Cha Tae Gwan <chataeg@gmail.com>
2025-04-01 15:31:28 +0900

    feat: add MissileFactory

---
+++ Strikers1945/MissileFactory.cpp
@@ -0,0 +1,37 @@
+#include "MissileFactory.h"
+
+void MissileFactory::AddMissile(FPOINT pos)
+{
+	Missile* missile = CreateMissile();
+
+	missile->Init();
+	missile->SetPos(pos);
+	missile->SetRender(true);
+	missile->SetActive(true);
+
+	missiles.push_back(missile);
+
+	//missile->Notice();
+
+
+}
+
+Missile* PlayerNormalMissileFactory::CreateMissile()
+{
+	return new PlayerNormalMissile();
+}
+
+Missile* PlayerBombMissileFactory::CreateMissile()
+{
+	return new PlayerBombMissile();
+}
+
+Missile* EnemyNormalMissileFactory::CreateMissile()
+{
+	return new EnemyNormalMissile();
+}
+
+Missile* EnemyLargeMissileFactory::CreateMissile()
+{
+	return new EnemyLargeMissile();
+}
